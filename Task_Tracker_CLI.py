f59a96ef146a86c8d5f186a1d772c1d5472c287f
Kyaw Zaya Oo <kyawzayaroo.mf@gmail.com>
2026-08-15 22:44:55 +0630

    Complete Version

--- Task_Tracker_CLI.py
+++ Task_Tracker_CLI.py
@@ -1,23 +1,30 @@
 import json
 import datetime
 
-
+#to open the json file in different functions
+def open_json_file():
+    try:
+        with open("task_data.json", "r") as file:
+            tasks = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        tasks = []
+    return tasks
+#adding tasks 
 def add():
+    #check the user's input
     while True:
         task_description = input('Task Description - ')
         if task_description:
             break
         else:
             print('Task Description should not be empty!')
-    try:
-        with open("task_data.json", "r") as file:
-            tasks = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        tasks = []
 
-    data = {"id": len(tasks) + 1,
+    #Opening the Json file by using the function.........
+    tasks = open_json_file()
+
+    data = {"id": len(tasks) + 1, #I think assiging the ID will be the problem when we delete one task. Will be great if it updates along with the list
             "description":task_description,
-            "Status": "todo",
+            "Status": "to do",
             "createAt": f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}",
             "updateAt": None}
 
@@ -35,25 +42,19 @@ def to_mark_progress():
             break
         else:
             print('Id is not valid')
-    try:
-        with open("task_data.json", "r") as file:
-            tasks = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        tasks = []
-
-    if id <= len(tasks):
-        for task in tasks:
-            for values in task.values():
-                if values == id:
-                    tasks[id-1]["Status"] = "in progress"
-                    tasks[id-1]["updateAt"] = f"{datetime.datetime.now()}"
-                    with open("task_data.json", "w") as file:
-                        json.dump(tasks, file, indent=4)
-                    print(f'Marked the task {tasks[id-1]["description"]} as in progress')
-                    break
-    else:
-        print('ID is not vilid.')
-
+            
+    tasks = open_json_file()
+    #iteration the list and ......
+    for task in tasks:
+        for values in task.values(): #since i stored the json file like this [{'id:2','discroption': 'Task name",....} , {'id':3,'discription':...}]
+            if values == id:
+                tasks[id-1]["Status"] = "in progress"
+                tasks[id-1]["updateAt"] = f"{datetime.datetime.now()}"
+                with open("task_data.json", "w") as file:
+                    json.dump(tasks, file, indent=4)
+                print(f'Marked the task {tasks[id-1]["description"]} as in progress')
+                break
+#the same with to_mark_progress function
 def to_mark_done():
     while True:
         id = input('Task ID: ')
@@ -62,31 +63,108 @@ def to_mark_done():
             break
         else:
             print('Id is not valid')
-    try:
-        with open("task_data.json", "r") as file:
-            tasks = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        tasks = []
 
-    if id <= len(tasks):
-        for task in tasks:
-            for keys,values in task.items():
-                if values == id:
-                    tasks[id-1]["Status"] = "done"
-                    tasks[id-1]["updateAt"] = f"{datetime.datetime.now()}"
-                    with open("task_data.json", "w") as file:
-                        json.dump(tasks, file, indent=4)
-                    print(f'Marked the task {tasks[id-1]["description"]} as done')
-                    break
-    else:
-        print('ID is not vilid.')
+    tasks = open_json_file()
 
-def delete():
-    pass
+    for task in tasks:
+        for keys,values in task.items():
+            if values == id:
+                task["Status"] = "done"
+                task["updateAt"] = f"{datetime.datetime.now()}"
+                with open("task_data.json", "w") as file:
+                    json.dump(tasks, file, indent=4)
+                print(f'Marked the task {tasks[id-1]["description"]} as done')
+                break
 
 def display():
-    pass
 
+    def display_done_tasks():
+        done_list = []
+        tasks = open_json_file()
+        for task in tasks:
+            for keys,value in task.items():
+                if value == "done":
+                    done_list.append(task['description']) 
+        if done_list:            
+            for n,task in enumerate(done_list, start=1):
+                print(n,'.', task)
+        else:
+            print('There is no done tasks.')
+
+    def display_inprogress_tasks():
+        inprogress_list = []
+        tasks = open_json_file()
+        for task in tasks:
+            for keys,value in task.items():
+                if value == "in progress":
+                    inprogress_list.append(task['description'])
+
+        if inprogress_list:            
+            for n,task in enumerate(inprogress_list, start=1):
+                print(n,'.', task)
+        else:
+            print('There is no in progress tasks.')                    
+    def display_todo_tasks():
+        todo_list = []
+        tasks = open_json_file()
+        for task in tasks:
+            for keys,value in task.items():
+                if value == "to do":
+                    todo_list.append(task['description'])
+        if todo_list:            
+            for n,task in enumerate(todo_list, start=1):
+                print(n,'.', task)
+        else:
+            print('There is no todo tasks.')
+
+    print("1.Display Done Tasks'\n2.Display Todo Tasks\n3.Display In-progress Tasks")
+    user_choice = input('Choose 1 to 3.')
+    if user_choice == '1':
+        display_done_tasks()
+    elif user_choice == '2':
+        display_todo_tasks()
+
+    elif user_choice == '3':
+        display_inprogress_tasks()
+
+    else:
+        print(f'{user_choice} is not valid. Please choose from 1 to 3.')
+
+def update():
+    while True:
+        user_input = input('Task ID: ')
+        if user_input and user_input.isdigit():
+            user_input = int(user_input)
+            break
+        else:
+            print('Id is not valid')
+
+    new_description = input('Update Task: ')
+
+    tasks = open_json_file()
+
+    for task in tasks:
+        for keys,values in task.items():
+            if values == user_input:
+                task['description'] = new_description
+                task["updateAt"] = f"{datetime.datetime.now()}"
+                with open("task_data.json", "w") as file:
+                    json.dump(tasks, file, indent=4)
+
+def delete():
+    while True:
+        id = input('Task ID: ')
+        if id and id.isdigit():
+            id = int(id)
+            break
+        else:
+            print('Id is not valid')
+    tasks = open_json_file()
+    for task in tasks:
+        for keys,values in task.items():
+            if values == id:
+                del task
+                print(f'{task} has been deleted.')
 
 def main():
     is_running = True
@@ -95,8 +173,9 @@ def main():
             "\n2. Add Task " \
             "\n3. To mark in Progress " \
             "\n4. To mark Done " \
-            "\n5. To delete" )
-        user_choice = input('Choose from 1 to 5')
+            "\n5. To delete"\
+            "\n6. To update the task" )
+        user_choice = input('Choose from 1 to 6')
         if user_choice:
             if user_choice == '1':
                 display()
@@ -110,6 +189,9 @@ def main():
 
             elif user_choice == '5':
                 delete()
+
+            elif user_choice == '6':
+                update()
 
             else:
                 print('Please choose form 1 to 5.')
